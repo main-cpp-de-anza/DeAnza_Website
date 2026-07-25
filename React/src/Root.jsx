3623224ca32d325b5d0891d50842960107dec01c
Hoai Phong - Alex <alexhoaiphong@gmail.com>
2026-07-24 19:26:35 -0700

    update preview

--- React/src/Root.jsx
+++ React/src/Root.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 
 export default function Root() {
-  return <BrowserRouter><App /></BrowserRouter>
+  return <HashRouter><App /></HashRouter>
 }
--- React/vite.config.js
+++ React/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/DeAnza_Website',
   plugins: [react()],
   test: {
     environment: 'jsdom',
